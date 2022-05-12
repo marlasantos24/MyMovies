309de80690e6d8580bc865493ebea991270230a2
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-05-11 21:58:12 -0500

    Layouts

--- app/src/main/java/com/example/mymoviesMS/MainActivity.kt
+++ app/src/main/java/com/example/mymoviesMS/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.mymoviesMS
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mymoviesMS.ui.theme.MyMoviesTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +34,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MyMoviesTheme {
-        Greeting("Android")
+fun MediaItem(){
+    Column() {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(Color.Red)
+        ) {
+            
+        }
+        Text("Title 1")
     }
 }
